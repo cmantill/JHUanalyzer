abe499ec8f263c2917dc924803aa33bcf643b50d
Cristina <criss.ms7@gmail.com>
2020-12-03 12:30:49 -0600

    more compatibility w sl7

--- condor/run_scripts/run_presel_ZbbSM.sh
+++ condor/run_scripts/run_presel_ZbbSM.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/10XwithNano.tgz ./
-export SCRAM_ARCH=slc6_amd64_gcc700
-scramv1 project CMSSW CMSSW_10_2_13
+export SCRAM_ARCH=slc7_amd64_gcc820
+scramv1 project CMSSW CMSSW_10_6_14
 tar xzf 10XwithNano.tgz
 rm 10XwithNano.tgz
 
 mkdir tardir; cp tarball.tgz tardir/; cd tardir
 tar xzvf tarball.tgz
-cp -r * ../CMSSW_10_2_13/src/JHUanalyzer/
-cd ../CMSSW_10_2_13/src/JHUanalyzer/
+cp -r * ../CMSSW_10_6_14/src/
+cd ../CMSSW_10_6_14/src/
 eval `scramv1 runtime -sh`
 
 echo make_preselection_HbbSM.py $*
 echo $*
 python make_preselection_HbbSM.py $*
 ls -lrth
-cp HbbSM*.root ../../../
+xrdcp HbbSM*.root root://cmseos.fnal.gov//store/user/cmantill/hbbSM/
--- make_preselection_HbbSM.py
+++ make_preselection_HbbSM.py
@@ -5,7 +5,6 @@
 #################################################################
 
 import ROOT
-from ROOT import *
 
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection,Object,Event
 from PhysicsTools.NanoAODTools.postprocessing.framework.treeReaderArrayTools import InputTree
@@ -17,12 +16,12 @@ import pickle
 from optparse import OptionParser
 import copy
 import math
-from math import sqrt
 import sys
 import time
 
 import Presel_Functions
-from Presel_Functions import *
+import array
+from array import array
 
 if __name__ == "__main__":
     
@@ -130,10 +129,10 @@ if __name__ == "__main__":
     #################################
     # Load cut values and constants #
     #################################
-    Cons = LoadConstants(options.year)
+    Cons = Presel_Functions.LoadConstants(options.year)
     lumi = Cons['lumi']
 
-    Cuts = LoadCuts(options.region,options.year)
+    Cuts = Presel_Functions.LoadCuts(options.region,options.year)
 
     #################################
     # Load N2ddt distribution
@@ -246,9 +245,9 @@ if __name__ == "__main__":
     # Make new file for storage #
     #############################
     if jobs!=1:
-        f = TFile( "HbbSM"+options.year+"_"+options.set+"_job"+options.job+"of"+options.njobs+mod+'_'+doubleB_short+'_'+options.region+".root", "recreate" )
+        f = ROOT.TFile( "HbbSM"+options.year+"_"+options.set+"_job"+options.job+"of"+options.njobs+mod+'_'+doubleB_short+'_'+options.region+".root", "recreate" )
     else:
-        f = TFile( "HbbSM"+options.year+"_"+options.set+mod+'_'+doubleB_short+'_'+options.region+".root", "recreate" )
+        f = ROOT.TFile( "HbbSM"+options.year+"_"+options.set+mod+'_'+doubleB_short+'_'+options.region+".root", "recreate" )
     f.cd()
 
     ###################
@@ -258,7 +257,7 @@ if __name__ == "__main__":
         hname = names[options.set.replace('ext','')]
     else:
         hname = 'data_obs'
-    Hbb_cutflow = TH1D('%s_cutflow'%hname, 'Hbb_cutflow', 10, 0.5, 10.5)
+    Hbb_cutflow = ROOT.TH1D('%s_cutflow'%hname, 'Hbb_cutflow', 10, 0.5, 10.5)
     Hbb_cutflow.GetXaxis().SetBinLabel(1, "no cuts")
     Hbb_cutflow.GetXaxis().SetBinLabel(2, "p_{T}")
     Hbb_cutflow.GetXaxis().SetBinLabel(3, "mass")
@@ -270,51 +269,51 @@ if __name__ == "__main__":
     Hbb_cutflow.GetXaxis().SetBinLabel(9, "M - "+doubleB_title)
     Hbb_cutflow.GetXaxis().SetBinLabel(10, "T - "+doubleB_title)
 
-    N2_map = TH3F('%s_n2ddt_map'%hname,'%s_n2ddt_map'%hname,
+    N2_map = ROOT.TH3F('%s_n2ddt_map'%hname,'%s_n2ddt_map'%hname,
                   30, -6, -1.5,
                   100, 400, 1200,
                   200, 0, 0.5)
     N2_map.Sumw2()
-    Hbb_doubleB = TH1F('%s_doubleB'%hname,''+doubleB_title+' tag',40,0,1)
+    Hbb_doubleB = ROOT.TH1F('%s_doubleB'%hname,''+doubleB_title+' tag',40,0,1)
     Hbb_doubleB.Sumw2()
-    Hbb_deepAK8 = TH1F('%s_deepAK8'%hname,'deepAK8', 40,0,1)
+    Hbb_deepAK8 = ROOT.TH1F('%s_deepAK8'%hname,'deepAK8', 40,0,1)
     Hbb_deepAK8.Sumw2()
-    Hbb_pT = TH1F('%s_pT'%hname,'hbb pT',40,200,1000)
+    Hbb_pT = ROOT.TH1F('%s_pT'%hname,'hbb pT',40,200,1000)
     Hbb_pT.Sumw2()
-    Hbb_pT_450 = TH1F('%s_pT_450'%hname,'hbb pT 450',40,400,1000)
+    Hbb_pT_450 = ROOT.TH1F('%s_pT_450'%hname,'hbb pT 450',40,400,1000)
     Hbb_pT_450.Sumw2()
-    Hbb_mSD = TH1F('%s_mSD'%hname,'hbb mSD',20,40,220)
+    Hbb_mSD = ROOT.TH1F('%s_mSD'%hname,'hbb mSD',20,40,220)
     Hbb_mSD.Sumw2()
-    Hbb_mSD_450 = TH1F('%s_mSD_450'%hname,'hbb mSD 450',20,40,220)
+    Hbb_mSD_450 = ROOT.TH1F('%s_mSD_450'%hname,'hbb mSD 450',20,40,220)
     Hbb_mSD_450.Sumw2()
-    Hbb_mSD_uncorr =  TH1F('%s_mSD_uncorr'%hname,'hbb mSD uncorr',20,40,220)
+    Hbb_mSD_uncorr =  ROOT.TH1F('%s_mSD_uncorr'%hname,'hbb mSD uncorr',20,40,220)
     Hbb_mSD.Sumw2()
-    Hbb_mSD_passM_n2ddt = TH1F('%s_mSD_passM_n2ddt'%hname,'hbb mSD',20,40,220)
+    Hbb_mSD_passM_n2ddt = ROOT.TH1F('%s_mSD_passM_n2ddt'%hname,'hbb mSD',20,40,220)
     Hbb_mSD.Sumw2()
-    Hbb_mSD_failM_n2ddt = TH1F('%s_mSD_failM_n2ddt'%hname,'hbb mSD',20,40,220)
+    Hbb_mSD_failM_n2ddt = ROOT.TH1F('%s_mSD_failM_n2ddt'%hname,'hbb mSD',20,40,220)
     Hbb_mSD.Sumw2()
-    Hbb_eta = TH1F('%s_eta'%hname,'hbb eta',40,-3,3)
+    Hbb_eta = ROOT.TH1F('%s_eta'%hname,'hbb eta',40,-3,3)
     Hbb_eta.Sumw2()
-    Hbb_rho = TH1F('%s_rho'%hname,'hbb rho',40,-7,-1)
+    Hbb_rho = ROOT.TH1F('%s_rho'%hname,'hbb rho',40,-7,-1)
     Hbb_rho.Sumw2()
-    Hbb_n2ddt_new = TH1F('%s_n2ddt_new'%hname,'hbb n2ddt new',40,-0.5,0.5)
+    Hbb_n2ddt_new = ROOT.TH1F('%s_n2ddt_new'%hname,'hbb n2ddt new',40,-0.5,0.5)
     Hbb_n2ddt_new.Sumw2()
-    Hbb_n2 = TH1F('%s_n2'%hname,'hbb n2',40,0.,0.8)
+    Hbb_n2 = ROOT.TH1F('%s_n2'%hname,'hbb n2',40,0.,0.8)
     Hbb_n2.Sumw2()
-    Muon_pT = TH1F('%s_muonpT'%hname,'muon pT',40,0,500)
+    Muon_pT = ROOT.TH1F('%s_muonpT'%hname,'muon pT',40,0,500)
     Muon_pT.Sumw2()
-    Muon_eta = TH1F('%s_muoneta'%hname,'muon eta',40,-2.5,2.5)
+    Muon_eta = ROOT.TH1F('%s_muoneta'%hname,'muon eta',40,-2.5,2.5)
     Muon_eta.Sumw2()
-    Muon_phi = TH1F('%s_muonphi'%hname,'muon phi',40,-4,4)
+    Muon_phi = ROOT.TH1F('%s_muonphi'%hname,'muon phi',40,-4,4)
     Muon_phi.Sumw2()
-    Muon_dphi = TH1F('%s_muondphi'%hname,'muon dphi',40,-4,4)
+    Muon_dphi = ROOT.TH1F('%s_muondphi'%hname,'muon dphi',40,-4,4)
     Muon_dphi.Sumw2()
-    Muon_pfRelIso04_all = TH1F('%s_muonpfRelIso04'%hname,'muon reliso 0.4',40,0,1)
+    Muon_pfRelIso04_all = ROOT.TH1F('%s_muonpfRelIso04'%hname,'muon reliso 0.4',40,0,1)
     Muon_pfRelIso04_all.Sumw2()
-    MET = TH1F('%s_MET'%hname,'MET',60,0,500)
+    MET = ROOT.TH1F('%s_MET'%hname,'MET',60,0,500)
     MET.Sumw2()
 
-    nev = TH1F("nev",   "nev",      1, 0, 1 )
+    nev = ROOT.TH1F("nev",   "nev",      1, 0, 1 )
 
     Mass_binBoundaries = []
     for i0 in range(0, 24):
@@ -334,7 +333,7 @@ if __name__ == "__main__":
             if options.JER != 'nominal': 
                 histname += '_JER%s'%options.JER.capitalize()
                 histtitle += '_JER%s'%options.JER.capitalize()
-            hists[wp][cat] = TH1F(histname, histtitle, len(Mass_binBoundaries)-1, array('d', Mass_binBoundaries))
+            hists[wp][cat] = ROOT.TH1F(histname, histtitle, len(Mass_binBoundaries)-1, array('d', Mass_binBoundaries))
 
     if runOthers == True:
         if not isData:
@@ -342,15 +341,15 @@ if __name__ == "__main__":
             for wp in WPs:
                 for sys in systs:
                     for cat in passfail:
-                        hists[wp]['%s_%s'%(cat,sys)] = TH1F("%s_%s%s_%s"%(hname,wp,cat,sys), "mass of Hbb vs pT - %s %s %s"%(cat,sys,wp), len(Mass_binBoundaries)-1,  array('d', Mass_binBoundaries))
+                        hists[wp]['%s_%s'%(cat,sys)] = ROOT.TH1F("%s_%s%s_%s"%(hname,wp,cat,sys), "mass of Hbb vs pT - %s %s %s"%(cat,sys,wp), len(Mass_binBoundaries)-1,  array('d', Mass_binBoundaries))
                         hists[wp]['%s_%s'%(cat,sys)].Sumw2()  
 
     print("Histograms booked")
     ###############################
     # Grab root file that we want #
     ###############################
-    file_string = Load_jetNano(options.set,options.year)
-    file = TFile.Open(file_string)
+    file_string = Presel_Functions.Load_jetNano(options.set,options.year)
+    file = ROOT.TFile.Open(file_string)
     print("root file"+file_string+" loaded")
 
     ################################
@@ -497,7 +496,7 @@ if __name__ == "__main__":
                 muon_dphi = abs(deltaPhi(muon_phi,ak8JetsColl[0].phi))
                 if(muon_pt > 55) and (abs(muon_eta) < 2.1):
                     Hbbsel['muonkin'] = True
-                    if(muon_dphi > 2.0*TMath.Pi()/3.0):
+                    if(muon_dphi > 2.0*ROOT.TMath.Pi()/3.0):
                        Hbbsel['muon'] = True
 
         # check Jet
@@ -508,46 +507,58 @@ if __name__ == "__main__":
 
         # mass selection
         #Hbbmsd = ak8JetsColl[0].msoftdrop
-        Hbbmsd = CorrectMSD(ak8JetsColl[0],ak8subJetsColl,puppisd_corrGEN,puppisd_corrRECO_cen,puppisd_corrRECO_for)
+        Hbbmsd = Presel_Functions.CorrectMSD(ak8JetsColl[0],ak8subJetsColl,puppisd_corrGEN,puppisd_corrRECO_cen,puppisd_corrRECO_for)
         if Hbbmsd < 20: continue
         Hbbsel['msd'] = Hbbmsd > 40
 
         # eta - this is ok as a cut
         if abs(ak8JetsColl[0].eta) > 2.4: continue
 
-        #jet0 = TLorentzVector(); jet0.SetPtEtaPhiM(ak8JetsColl[0].pt_nom, ak8JetsColl[0].eta, ak8JetsColl[0].phi, Hbbmsd)
-        jet0 = TLorentzVector(); jet0.SetPtEtaPhiM(ak8JetsColl[0].pt, ak8JetsColl[0].eta, ak8JetsColl[0].phi, Hbbmsd)
+        jet0 = ROOT.TLorentzVector();
+        jet0.SetPtEtaPhiM(ak8JetsColl[0].pt_nom, ak8JetsColl[0].eta,ak8JetsColl[0].phi, Hbbmsd)
+        # jet0 = TLorentzVector(); 
+        # jet0.SetPt(ak8JetsColl[0].pt)
+        # jet0.SetEta(ak8JetsColl[0].eta)
+        # jet0.SetPhi(ak8JetsColl[0].phi)
+        # jet0.SetM(Hbbmsd)
 
         # check AK4 jet
         Hbbsel['btag'] = False
         if len(ak4JetsColl)>0:
             for i in range(0,len(ak4JetsColl)):
                 if ak4JetsColl[i].btagDeepB > 0.6324 and ak4JetsColl[i].pt > 50 and abs(ak4JetsColl[i].eta) < 2.5:
-                    ak4vec =  TLorentzVector(); ak4vec.SetPtEtaPhiM(ak4JetsColl[i].pt,ak4JetsColl[i].eta,ak4JetsColl[i].phi,ak4JetsColl[i].mass);
+                    ak4vec =  ROOT.TLorentzVector(); 
+                    ak4vec.SetPtEtaPhiM(ak4JetsColl[i].pt,ak4JetsColl[i].eta,ak4JetsColl[i].phi,ak4JetsColl[i].mass)
+                    #ak4vec.SetPt(ak4JetsColl[i].pt)
+                    #ak4vec.SetEta(ak4JetsColl[i].eta)
+                    #ak4vec.SetPhi(ak4JetsColl[i].phi)
+                    #ak4vec.SetM(ak4JetsColl[i].mass)
                     if(ak4vec.DeltaR(jet0) > 0.8):
                         Hbbsel['btag'] = True
 
         # rho
         if ak8JetsColl[0].pt >= 200:
-            Hbbrho = 2*log(jet0.M()/jet0.Pt())
+            Hbbrho = 2*math.log(jet0.M()/jet0.Pt())
             Hbbsel['rho'] = -6 < Hbbrho < -2.1
         else:
             Hbbrho = -1
             Hbbsel['rho'] = False
 
         # N2ddt cut
-        n2ddt_new = getN2dtt(trans_h2ddt_new,ak8JetsColl[0].n2b1,Hbbrho,Hbbpt)
+        n2ddt_new = Presel_Functions.getN2dtt(trans_h2ddt_new,ak8JetsColl[0].n2b1,Hbbrho,Hbbpt)
         Hbbsel['n2ddt_new'] = n2ddt_new < 0
 
         # match jet                                                                                                                                                             
         Hbbsel['unmatched'] = False
         Hbbsel['semimatched'] = False
         Hbbsel['matched'] = False
+        vid=0
+        genVPt =0
         if not isData:
             if 'zqq' in names[options.set.replace('ext','')]: vid = 23
             if 'wqq' in names[options.set.replace('ext','')]: vid = 24
             if 'tqq' in names[options.set.replace('ext','')]:
-                matched = TopJetMatching(jet0, GenParticles)
+                matched = Presel_Functions.TopJetMatching(jet0, GenParticles)
                 if matched==3:
                     Hbbsel['matched'] = True
                 elif matched==2:
@@ -555,11 +566,12 @@ if __name__ == "__main__":
                 else:
                     Hbbsel['unmatched'] = True
             else:
-                matched,genVPt = VJetMatching(jet0,GenParticles,vid)
-                if matched:
-                    Hbbsel['matched'] = True
-                else:
-                    Hbbsel['unmatched'] = True
+                if vid>0:
+                    matched,genVPt = Presel_Functions.VJetMatching(jet0,GenParticles,vid)
+                    if matched:
+                        Hbbsel['matched'] = True
+                    else:
+                        Hbbsel['unmatched'] = True
 
         # bb selection
         Hbbsel['DoubleB_lead_tight'] = (Cuts['doublebtagTight'][0] < getattr(ak8JetsColl[0],doubleB_name) < Cuts['doublebtagTight'][1])
@@ -629,7 +641,7 @@ if __name__ == "__main__":
             if muon_pt>0 and abs(muon_eta)>0:
                 muPtForTrig = muon_pt
                 muEtaForTrig = abs(muon_eta)
-                muTrigs = Hist2d_Lookup(muPtForTrig,muEtaForTrig,mutrig_eff)
+                muTrigs = Presel_Functions.Hist2d_Lookup(muPtForTrig,muEtaForTrig,mutrig_eff)
 
                 weights['mutrigger']['nom'] = muTrigs[0]
                 weights['mutrigger']['up'] = muTrigs[1]
@@ -637,12 +649,12 @@ if __name__ == "__main__":
 
                 muPtForId = max(20., min(muon_pt, 100.))
                 muEtaForId = min(abs(muon_eta), 2.3)
-                muIds = Hist2d_Lookup(muPtForId,muEtaForId,muid_eff)
+                muIds = Presel_Functions.Hist2d_Lookup(muPtForId,muEtaForId,muid_eff)
                 weights['muid']['nom'] = muIds[0]
                 weights['muid']['up'] = muIds[1]
                 weights['muid']['down'] = muIds[2]
 
-                muIsos = Hist2d_Lookup(muPtForId,muEtaForId,muiso_eff)
+                muIsos = Presel_Functions.Hist2d_Lookup(muPtForId,muEtaForId,muiso_eff)
                 weights['muiso']['nom'] = muIsos[0]
                 weights['muiso']['up'] = muIsos[1]
                 weights['muiso']['down'] = muIsos[2]
@@ -660,10 +672,10 @@ if __name__ == "__main__":
         #########################################
         # Fill control histograms #
         #########################################
-        nominalweight = Weightify(weights,'nominal',['Pu','mutrigger','muid','muiso'])
+        nominalweight = Presel_Functions.Weightify(weights,'nominal',['Pu','mutrigger','muid','muiso'])
         if not isData:
             if ak8JetsColl[0].msoftdrop > 20 and ak8JetsColl[0].pt > 200 and not Hbbsel['leptonExists']:
-                N2_map.Fill(2*log(ak8JetsColl[0].msoftdrop/ak8JetsColl[0].pt),ak8JetsColl[0].pt,ak8JetsColl[0].n2b1,norm_weight*nominalweight)
+                N2_map.Fill(2*math.log(ak8JetsColl[0].msoftdrop/ak8JetsColl[0].pt),ak8JetsColl[0].pt,ak8JetsColl[0].n2b1,norm_weight*nominalweight)
         if preselection:
             Hbb_rho.Fill(Hbbrho,norm_weight*nominalweight)
             Hbb_pT.Fill(jet0.Pt(),norm_weight*nominalweight)
@@ -709,8 +721,8 @@ if __name__ == "__main__":
                                 hists[wp]['%s_%s'%(cat,sys)].Fill(mass_corr,norm_weight*nominalweight)
                         for sys in ['Pu','mutrigger','muiso','muid']:
                             if Hbbsel['%s_%s'%(cat,wp)]:
-                                hists[wp]['%s_%sUp'%(cat,sys)].Fill(jet0.M(),norm_weight*Weightify(weights,'%s_up'%sys,['Pu','mutrigger','muid','muiso']))
-                                hists[wp]['%s_%sDown'%(cat,sys)].Fill(jet0.M(),norm_weight*Weightify(weights,'%s_down'%sys,['Pu','mutrigger','muid','muiso']))
+                                hists[wp]['%s_%sUp'%(cat,sys)].Fill(jet0.M(),norm_weight*Presel_Functions.Weightify(weights,'%s_up'%sys,['Pu','mutrigger','muid','muiso']))
+                                hists[wp]['%s_%sDown'%(cat,sys)].Fill(jet0.M(),norm_weight*Presel_Functions.Weightify(weights,'%s_down'%sys,['Pu','mutrigger','muid','muiso']))
 
     end = time.time()
     print '\n'
